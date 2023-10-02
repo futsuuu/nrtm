1d820fdeeae333fce1e08046a14deac2ad349492
futsuuu <futsuuu123@gmail.com>
2023-10-02 13:03:13 +0900

    feat: extract downloaded archive

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::env;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
+use anyhow::Context as _;
 use clap::{Parser, Subcommand};
+use futures_util::{future, StreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::Client;
-use tokio::{fs::File, io::AsyncWriteExt};
-use tokio_stream::StreamExt;
+use tokio::{
+    fs::{create_dir_all, File},
+    io::AsyncWriteExt,
+};
+
+#[cfg(target_os = "windows")]
+const ARCHIVE_EXT: &str = "zip";
+#[cfg(not(target_os = "windows"))]
+const ARCHIVE_EXT: &str = "tar.gz";
 
 /// A runtime manager for Neovim
 #[derive(Parser)]
@@ -24,30 +35,61 @@ enum Commands {
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
+    let base_dir = match env::var("NRTM_DIR") {
+        Ok(d) => PathBuf::from(d),
+        Err(_) => home::home_dir()
+            .context("Failed to get home directory.")?
+            .join(".nrtm"),
+    };
+
+    // Initialize directory structure
+    let mut dirs =
+        future::join_all(["app", "bin", "cache"].map(|name| base_dir.join(name)).map(
+            |path| async move {
+                create_dir_all(&path).await?;
+                anyhow::Ok(path)
+            },
+        ))
+        .await
+        .into_iter();
+
+    let app_dir = dirs.next().unwrap()?;
+    let _bin_dir = dirs.next().unwrap()?;
+    let cache_dir = dirs.next().unwrap()?;
+
     let release_name = format!(
-        "nvim-{}",
+        "nvim-{}.{ARCHIVE_EXT}",
         match env::consts::OS {
-            "linux" => "linux64.tar.gz",
-            "macos" => "macos.tar.gz",
-            "windows" => "win64.zip",
+            "linux" => "linux64",
+            "macos" => "macos",
+            "windows" => "win64",
             _ => anyhow::bail!("Unsupported OS"),
         }
     );
 
     match &args.command {
         Commands::Get { version } => {
+            let download_target = cache_dir.join(format!("{version}.{ARCHIVE_EXT}"));
             download_file(
-                &Client::new(),
+                &reqwest::Client::new(),
                 &format!("https://github.com/neovim/neovim/releases/download/{version}/{release_name}"),
-                &release_name
+                &download_target,
             ).await?;
+            extract_archive(
+                std::fs::File::open(download_target)?,
+                app_dir.join(version),
+            )?;
         }
     }
 
     Ok(())
 }
 
-async fn download_file(client: &Client, url: &str, path: &str) -> anyhow::Result<()> {
+async fn download_file(
+    client: &reqwest::Client,
+    url: &str,
+    path: impl AsRef<Path>,
+) -> anyhow::Result<()> {
     let res = client.get(url).send().await?;
     let total_size = res.content_length().unwrap() as u64;
 
@@ -65,6 +107,68 @@ async fn download_file(client: &Client, url: &str, path: &str) -> anyhow::Result
         file.write_all(&chunk).await?;
         downloaded_size += chunk.len();
         pb.set_position(downloaded_size as u64);
+    }
+
+    Ok(())
+}
+
+fn extract_archive(archive: std::fs::File, target: PathBuf) -> anyhow::Result<()> {
+    std::fs::create_dir_all(&target)?;
+
+    fn strip_toplevel(rel_path: impl AsRef<Path>) -> anyhow::Result<PathBuf> {
+        let rel_path = rel_path.as_ref();
+        let prefix = rel_path.iter().next().unwrap();
+        let path = rel_path.strip_prefix(prefix)?.to_path_buf();
+        Ok(path)
+    }
+
+    if ARCHIVE_EXT == "zip" {
+        let mut archive = zip::ZipArchive::new(archive)?;
+
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i)?;
+            let rel_path = strip_toplevel(file.mangled_name())?;
+            let path = target.join(rel_path);
+
+            if file.is_dir() {
+                std::fs::create_dir_all(&path)?;
+            } else {
+                std::fs::create_dir_all(path.parent().unwrap())?;
+                let mut outfile = std::fs::File::create(&path)?;
+                std::io::copy(&mut file, &mut outfile)?;
+
+                #[cfg(unix)]
+                {
+                    use std::os::unix::fs::PermissionsExt;
+                    if let Some(mode) = file.unix_mode() {
+                        std::fs::set_permissions(
+                            &path,
+                            std::fs::Permissions::from_mode(mode),
+                        )?;
+                    }
+                }
+            }
+        }
+
+        return Ok(());
+    }
+
+    if ARCHIVE_EXT == "tar.gz" {
+        let tar = flate2::read::GzDecoder::new(archive);
+        let mut archive = tar::Archive::new(tar);
+
+        for file in archive.entries()? {
+            let Ok(mut file) = file else {
+                continue;
+            };
+
+            let rel_path = strip_toplevel(file.path()?)?;
+            let path = target.join(&rel_path);
+
+            file.unpack(&path)?;
+        }
+
+        return Ok(());
     }
 
     Ok(())
