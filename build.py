bcafed6c132fc78f7f63e74ae34000aba1d71ef8
futsuuu <futsuuu123@gmail.com>
2023-10-05 08:48:20 +0900

    feat(release): publish assets

--- build.py
+++ build.py
@@ -1,7 +1,32 @@
 """
 This is a wrapper of `cargo build` command.
-You can pass the `--release` flag, the `--target` flag, etc.
-If you pass the `--dist` flag, `out.zip` or `out.tar.gz` will be created.
+
+1> build.py
+
+    out/
+     '-- bin/
+          '-- *.exe
+
+2> build.py --dist
+
+    nrtm.zip
+     '-- nrtm/
+          '-- bin/
+               '-- *.exe
+
+3> build.py --target $target
+
+    out-{target}/
+     '-- bin/
+          '-- *.exe
+
+4> build.py --dist --target $target
+
+    nrtm-{target}.zip
+     '-- nrtm/
+           '-- bin/
+                '-- *.exe
+
 """
 
 import json
@@ -13,21 +38,23 @@ import subprocess
 import sys
 
 
-ARCHIVE_NAME = "out"
+OUT_DIR = "out"
+DIST_NAME = "nrtm"
 DIST_FLAG = "--dist"
+TARGET_FLAG = "--target"
 
 
-def make_archive(directory: str) -> str:
-    archive_format = "zip" if platform.system() == "Windows" else "gztar"
+def create_dist(base_name: str, base_dir: str):
+    dist_format = "zip" if platform.system() == "Windows" else "gztar"
     shutil.make_archive(
-        ARCHIVE_NAME,
-        archive_format,
+        base_name,
+        dist_format,
         root_dir=".",
-        base_dir=directory,
+        base_dir=base_dir,
     )
 
-    archive_name = ARCHIVE_NAME + (".zip" if archive_format == "zip" else ".tar.gz")
-    print(f"./{directory}/ --> ./{archive_name}  # {get_size(archive_name)} KB")
+    dist_name = DIST_NAME + (".zip" if dist_format == "zip" else ".tar.gz")
+    print(f"./{base_dir}/ --> ./{dist_name}  # {get_size(dist_name)} KB")
 
 
 def get_size(path: str) -> int:
@@ -39,14 +66,29 @@ def main():
 
     if dist := DIST_FLAG in args:
         args.remove(DIST_FLAG)
+        if "--release" not in args and "-r" not in args:
+            args.append("--release")
+
+    build_target = ""
+    if TARGET_FLAG in args:
+        build_target = args[args.index(TARGET_FLAG) + 1]
+        subprocess.run(["rustup", "target", "add", build_target])
+
+    # Install cargo-zigbuild if needed
+    build_command = "build"
+    if dist and "-musl" in build_target:
+        subprocess.run(["python", "-m", "pip", "install", "cargo-zigbuild"])
+        build_command = "zigbuild"
 
     result = subprocess.run(
-        ["cargo", "build", "--message-format", "json"] + args,
+        ["cargo", build_command, "--message-format", "json"] + args,
         stdout=subprocess.PIPE,
         encoding="utf-8",
     )
 
-    out_dir = "nrtm" if dist else ARCHIVE_NAME
+    out_dir = DIST_NAME if dist else OUT_DIR
+    if build_target:
+        out_dir += f"-{build_target}"
     bin_dir = os.path.join(out_dir, "bin")
 
     os.makedirs(bin_dir, exist_ok=True)
@@ -55,19 +97,27 @@ def main():
         if not line:
             continue
 
-        data = json.loads(line)
-        if exe := data.get("executable"):
-            basename = os.path.basename(exe)
-            # Rename `shim` to `nvim`, e.g. `shim.exe` -> `nvim.exe`
-            filename = re.sub(r"^shim((\.\w+)+)$", r"nvim\1", basename)
+        if not (exe := json.loads(line).get("executable")):
+            continue
 
-            target = os.path.join(bin_dir, filename)
-            shutil.copy(exe, target)
-            print(f"{exe} --> ./{target}  # {get_size(target)} KB")
+        basename = os.path.basename(exe)
+        # Rename `shim` to `nvim`, e.g. `shim.exe` -> `nvim.exe`
+        filename = re.sub(r"^shim((\.\w+)+)$", r"nvim\1", basename)
 
-    if dist:
-        make_archive(out_dir)
-        shutil.rmtree(out_dir)
+        target = os.path.join(bin_dir, filename)
+        shutil.copy2(exe, target)
+        print(f"{exe} --> ./{target}  # {get_size(target)} KB")
+
+    if not dist:
+        return
+
+    if build_target:
+        dist_file = f"{DIST_NAME}-{build_target}"
+    else:
+        dist_file = DIST_NAME
+
+    create_dist(dist_file, out_dir)
+    shutil.rmtree(out_dir)
 
 
 if __name__ == "__main__":
