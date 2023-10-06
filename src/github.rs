256599326adaa8f6223a4f8f8eaed4e0b28400ca
futsuuu <futsuuu123@gmail.com>
2023-10-06 19:34:25 +0900

    feat: parse Neovim versions from release note and name

--- src/github.rs
+++ src/github.rs
@@ -2,10 +2,12 @@ use std::{fs, path::PathBuf};
 
 use anyhow::Context as _;
 use once_cell::sync::Lazy;
+use regex::Regex;
 use reqwest::{
     header::{self, HeaderMap, HeaderValue},
     Client,
 };
+use semver::Version;
 use serde::Deserialize;
 
 use crate::CACHE_DIR;
@@ -46,24 +48,43 @@ pub async fn get_releases() -> anyhow::Result<Vec<Release>> {
 #[derive(Deserialize)]
 #[cfg_attr(test, derive(Default))]
 pub struct Release {
+    pub name: String,
     pub tag_name: String,
     pub body: String,
     pub assets: Vec<Asset>,
 }
 
 impl Release {
-    pub fn get_nvim_version(&self) -> anyhow::Result<String> {
-        let version = self
-            .body
-            .split_once("NVIM v")
-            .context("Failed to get Neovim version froma release note.")?
-            .1
-            .split_whitespace()
-            .next()
-            .unwrap();
-
-        Ok(version.to_string())
+    pub fn get_nvim_version(&self) -> anyhow::Result<Version> {
+        match get_nvim_version(&self.body) {
+            Ok(v) => Ok(v),
+            Err(_) => get_nvim_version(&self.name),
+        }
     }
+}
+
+fn get_nvim_version(text: &str) -> anyhow::Result<Version> {
+    static RE: Lazy<Regex> = Lazy::new(|| {
+        Regex::new(
+            r"(^|\W)(?i)nvim(?-i) v?(?<version>\d+\.\d+\.\d+(-\S+)*(\+\S+)*)(\W|$)",
+        )
+        .unwrap()
+    });
+
+    let caps = RE
+        .captures(text)
+        .with_context(|| format!("Failed to get Neovim version from text: {text}"))?;
+    let raw_version = caps.name("version").map_or("", |m| m.as_str());
+
+    let version = if let Some((version_prerelease, build_metadata)) =
+        raw_version.rsplit_once("-")
+    {
+        Version::parse(&format!("{version_prerelease}+{build_metadata}"))?
+    } else {
+        Version::parse(raw_version)?
+    };
+
+    Ok(version)
 }
 
 #[derive(Deserialize)]
@@ -90,14 +111,16 @@ fn new_client() -> anyhow::Result<Client> {
 
 #[cfg(test)]
 mod tests {
+    use semver::Version;
+
     use super::*;
 
     #[test]
     fn get_nvim_version() {
-        let mut release = Release::default();
-        release.body = "foo\n```\nNVIM v1.0.0-12-234 (bar)\n".into();
-        let version = release.get_nvim_version();
-        assert!(version.is_ok());
-        assert_eq!(version.unwrap(), "1.0.0-12-234".to_string());
+        let version = get_nvim_version("NVIM v1.0.0");
+        assert_eq!(version.unwrap(), Version::parse("1.0.0").unwrap());
+
+        let version = get_nvim_version("foo\n```\nNvim v1.0.0-dev-1234 (bar)\n");
+        assert_eq!(version.unwrap(), Version::parse("1.0.0-dev+1234").unwrap());
     }
 }
