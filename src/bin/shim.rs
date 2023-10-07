507d8fa1773ae6dd7261cfe62e5c07776436bb5f
futsuuu <futsuuu123@gmail.com>
2023-10-08 05:00:07 +0900

    feat(shim): by default, use Neovim which is installed on the system

--- src/bin/shim.rs
+++ src/bin/shim.rs
@@ -6,9 +6,9 @@ use std::{
 use nrtm::shim;
 
 fn main() -> anyhow::Result<()> {
-    let state = shim::State::new()?;
+    let state = shim::State::read()?;
     let mut command = Command::new(state.exe_path);
-    if !state.appname.is_empty() {
+    if !state.appname.is_empty() && env::var("NVIM_APPNAME").is_err() {
         command.env("NVIM_APPNAME", state.appname);
     }
     let exit_code = command.args(env::args_os().skip(1)).status()?.code();
